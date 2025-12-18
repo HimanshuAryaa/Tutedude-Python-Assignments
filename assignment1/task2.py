f9671692b02a0e89868d80d6d53f66b48caebcf5
Himanshu Arya <49977125+HimanshuAryaa@users.noreply.github.com>
2025-12-18 17:38:42 +0530

    Create task2.py

---
+++ assignment1/task2.py
@@ -0,0 +1,8 @@
+# Task 2: Create a Personalized Greeting
+
+first_name = input("Enter the first name: ")
+last_name = input("Enter the last name: ")
+
+full_name = first_name + " " + last_name
+
+print(f"\nHello, {full_name}! Welcome to the python program.")
