c86adff36cd7a67b7965ef19201dc8f41e26d545
Himanshu Arya <49977125+HimanshuAryaa@users.noreply.github.com>
2025-12-28 07:47:46 +0530

    Create task2.py

---
+++ assignment3/task2.py
@@ -0,0 +1,13 @@
+# Task 2: Using the Math Module for Calculations
+
+import math
+
+num = int(input("Enter a number: "))
+
+square = math.sqrt(num)
+log = math.log(num)
+sin = math.sin(num)
+
+print(f"Square root: {square}")
+print(f"Logarithm: {log}")
+print(f"Sine: {sin}")
