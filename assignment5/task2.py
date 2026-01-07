9eb05385dc9234dd94ad51f554f748b1d4184fa0
Himanshu Arya <49977125+HimanshuAryaa@users.noreply.github.com>
2026-01-07 18:09:49 +0530

    Create task2.py

---
+++ assignment5/task2.py
@@ -0,0 +1,10 @@
+# Task 2: Demonstrate List Slicing
+
+item_lst = [1,2,3,4,5,6,7,8,9,10]
+
+extract_lst = item_lst[0:5]
+reversed_lst = extract_lst[::-1]
+
+print(f"Original list: {item_lst}")
+print(f"Extracted first five elements: {extract_lst}")
+print(f"Reversed extracted elements: {reversed_lst}")
